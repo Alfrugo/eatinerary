33cb22bfcfb67b126e5d64f0734eb7b88c31824c
Alfrugo <adiez@mtm-inc.net>
2022-04-12 20:04:37 -0500

    heroku deploy

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+​
+​
+const app = express();
+const PORT = process.env.PORT || 3001;
+​
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+​
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+​
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+});
+​
+​
+app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+})
